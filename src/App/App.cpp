672d76992904bd67d94b7d855854a97a9e13f68b
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2025-07-03 19:07:08 +0300

    Implement Main Loop in App.h

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -19,19 +19,25 @@ bool App::Init(uint32_t height, uint32_t width, uint8_t zoom)
 
 void App::Run()
 {
-	screen_.RenderScreen();
-	
+
+
+
 	Triangle2D triangle(Vector2D(10, 10), Vector2D(20, 10), Vector2D(30, 40));
 	Rectangle2D rectangle(Vector2D(50, 50), Vector2D(100, 100));
-	screen_.Draw(&rectangle, Color::Blue(), true, Color::Blue(), true);
-	screen_.Draw(&triangle, Color::Green(), true, Color::Green(), true);
 
-	screen_.RenderScreen();
+
+	Uint64 currentTime = SDL_GetTicks64();
+	const Uint8 fixedDeltaTime = 10;
+	Uint64 accumulator = 0.0;
+
+	
 
 	SDL_Event sdlEvent;
 	bool running = true;
 	while (running)
 	{
+
+		//input
 		while (SDL_PollEvent(&sdlEvent))
 		{
 			switch (sdlEvent.type)
@@ -51,8 +57,39 @@ void App::Run()
 			}
 			}
 		}
-	}
 
+		//update
+		Uint64 newTime = SDL_GetTicks64();
+		Uint64 frameTime = newTime - currentTime;
+		double deltaTime = frameTime / 1000.0;
+
+		if(frameTime > 250)
+		{
+			frameTime = 250; // cap the frame time to prevent large jumps
+		}
+
+		currentTime = newTime;
+		accumulator += frameTime;
+	    
+		std::cout << "Delta time is : " << deltaTime << std::endl;
+		std::cout << "Acummulator is : " << accumulator <<" So it will loop "<< static_cast<int>(accumulator / fixedDeltaTime) << " times" << std::endl;
+		while (accumulator >= fixedDeltaTime)
+		{
+			//update logic here
+			accumulator -= fixedDeltaTime;
+			std::cout << "Fixed update logic here " <<fixedDeltaTime /1000.0<< std::endl;
+
+		}	
+
+
+		//render
+		screen_.Draw(&rectangle, Color::Blue(), true, Color::Blue(), true);
+		screen_.Draw(&triangle, Color::Green(), true, Color::Green(), true);
+
+		screen_.RenderScreen();
+
+
+	}
 
 
 }
